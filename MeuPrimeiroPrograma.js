dac09e31da6fe0ce586edf88234e6759e2f247c7
Lucas Costa <88509075+lucascosta9349@users.noreply.github.com>
2023-02-14 10:48:54 -0300

    Usando variáveis no JS
    como usar variáveis

--- MeuPrimeiroPrograma.js
+++ MeuPrimeiroPrograma.js
@@ -1,1 +1,8 @@
-console.log("Meu Primeiro Programa");
+console.log("Meu Primeiro Programa: trabalhnado com variáveis");
+
+const idade = 26;
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
