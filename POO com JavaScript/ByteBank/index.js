f23844c690b71efcab3e522c237dca4df9d7afa5
Lucas Costa <88509075+lucascosta9349@users.noreply.github.com>
2023-03-07 12:30:36 -0300

    criando arquivos de classe

--- POO com JavaScript/ByteBank/index.js
+++ POO com JavaScript/ByteBank/index.js
@@ -1,39 +1,19 @@
-class Cliente {
-  nome
-  cpf
-  rg
-}
+const cliente1 = new Cliente()
+cliente1.nome = 'Ricardo'
+cliente1.cpf = 11122233309
 
-class ContaCorrente {
-  agencia
-  #saldo = 0
-
-  sacar(valor) {
-    if (this.#saldo >= valor) {
-      this.#saldo -= valor
-    }
-  }
-
-  depositar(valor) {
-    if (valor > 0) {
-      this.#saldo += valor
-      console.log(this.#saldo)
-    }
-  }
-}
-
-const Cliente1 = new Cliente()
-const Cliente2 = new Cliente()
-
-Cliente1.nome = 'Ricardo'
-Cliente1.cpf = 11122233309
-Cliente1.rg = 123456789
-
-Cliente2.nome = 'Alice'
-Cliente2.cpf = 88822233309
+const cliente2 = new Cliente()
+cliente2.nome = 'Alice'
+cliente2.cpf = 88822233309
 
 const contaCorrenteRicardo = new ContaCorrente()
 contaCorrenteRicardo.agencia = 1001
+
 contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+
+const valorSacado = contaCorrenteRicardo.sacar(50)
+console.log(valorSacado)
 
 console.log(contaCorrenteRicardo)
