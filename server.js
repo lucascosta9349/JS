74a4cca3f9782d6f47460a7562a2e1d70a7a74a1
lucas <lucascosta9349@gmail.com>
2026-07-14 15:32:05 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+/*3 rotas
+públicas
+    cadastro e Login
+
+Privadas
+    listar Usuários
+*/
+
+console.log("express")
+app.listen(3000, () => console.log("servidor Rodando"))
+
+
